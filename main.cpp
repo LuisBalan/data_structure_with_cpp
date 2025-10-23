0a1d2f299341a13cac75cacdbfce394d1404ca81
LuisBalan <jlbalcz@gmail.com>
2025-10-23 01:27:13 -0600

    avances

--- main.cpp
+++ main.cpp
@@ -7,24 +7,31 @@ int main()
     std::string segundo_nombre;
     std::string apellido_paterno;
     std::string apellido_materno;
+    int pasatiempo;
 
     int month, day, year;
 
-    std::cout << "Ingresa un primer nombre:";
+    std::cout << "Ingresa un primer nombre: " << std::endl;
     std::getline(std::cin, primer_nombre);
     // std::cin >> primer_nombre;
-    std::cout << "Si tienes un segundo nombre, escribelo: ";
+    std::cout << " Si tienes un segundo nombre, escribelo: " << std::endl;
     std::getline(std::cin, segundo_nombre);
-    std::cout << "Ingresa tu fecha de nacimiento";
-    std::cout << "Primero el mes: ";
-    std::getline(std::cin, month);
-    // std::getline(std::cin, day);
-    // std::getline(std::cin,  year);
+    std::cout << "Ingrese el apellido paterno: " << std::endl;
+    std::cin >> apellido_paterno;
+    std::cout << "Ingrese el apellido materno: " << std::endl;
+    std::cin >> apellido_materno;
+    std::cout << " Ingresa tu fecha de nacimiento: " << std::endl;
+    std::cout << " Primero el mes: " << std::endl;
+    std::cin >> month;
+    std::cout << "Ingresa el día: " << std::endl;
+    std::cin >> day;
+    std::cout << "Ahora ingresa el año de nacimiento: " << std::endl;
+    std::cin >> year;
+    std::cout << "¿Qué pasatiempo prefieres?: " << std::endl;
+    std::cout << "Op1: Escuchar música. " << std::endl;
+    std::cout << "Op2: Platicar con amigxs. " << std::endl;
+    std::cout << "Op3: Ver series. " << std::endl;
+    std::cin >> pasatiempo;
 
     return 0;
-
-
-
-
-
 }
